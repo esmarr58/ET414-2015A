c969adb5a218b9a094ef9af44ecc062012a53bd8
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-02-11 21:00:14 -0600

    Create plantilla.cpp

---
+++ S5/plantilla.cpp
@@ -0,0 +1,92 @@
+
+
+/*
+
+
+g++ -Wall -o manzana template.cpp `pkg-config --cflags --libs opencv`
+
+
+*/
+// cp /home/rub/ET414-B/Sesion2/ejemplo1.cpp /home/rub/ET414-B/Sesion3/template.cpp
+// cp -> permite copiar archivos
+// mkdir -> crear carpetas
+// cd .. -> bajar una carpeta anterior
+// cd -> subir una carpeta
+// gedit -> crear archivos con alguna extension determinada
+// locate -> buscar archivos
+// ls -> mostrar archivos y carpetas
+// pwd -> mostrar la ubicacion actual del cursos
+// sudo apt-get install programa
+
+/*
+	Este es el ejemplo1 de la sesion1
+	Muestra el uso  y declaracion de clases en c++
+*/
+
+#include<opencv2/core/core.hpp>
+#include<opencv2/ml/ml.hpp>
+#include<opencv/cv.h>
+#include<opencv2/imgproc/imgproc.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
+
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
+/*      Funcion SiRatonSeMueve. Cada vez que el raton se mueva
+	en la ventana "programada" se ejecutara esta funcion.
+
+	
+
+
+*/
+
+static void SiRatonSeMueve(	int evento, 
+				int x, 
+				int y, 
+				int flags, 
+				void* param){
+		
+		switch(evento)       
+		{
+			case CV_EVENT_LBUTTONDOWN :
+					cout <<"X: "<< x << endl;
+					cout <<"Y: "<< y << endl;
+					cout << "*********" << endl;
+					break;
+			default : 
+					break;
+		}
+}
+
+
+
+
+
+int main(void)
+{
+	int  dummy = 0;
+	Mat A(4,4,CV_8UC3,Scalar(0, 255, 0)); 
+
+	namedWindow("Principal",1);
+
+	cvSetMouseCallback(	"Principal",
+				SiRatonSeMueve, 
+				&dummy );
+
+
+	
+
+	for(;;)
+	{
+		imshow("Principal", A);
+	
+	if(waitKey(30)>0) break;
+
+ 	}
+
+	return 1;
+}
+
